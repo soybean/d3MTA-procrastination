f6623ba84bf9798acee9be893e7c0622d80c1efd
Melanie Sawyer <melaniensawyer@gmail.com>
2019-10-07 18:37:16 -0400

    updates

--- trainz.js
+++ trainz.js
@@ -1,14 +1,26 @@
 $(document).ready(function() {
+  var allTrains = ["1", "2", "3", "4", "5", "6", "A", "B", "C", "D", "E", "F"]
+  var selectedTrains = ["1", "2", "3", "4", "5", "6", "A", "B", "C", "D", "E", "F"]
   var canReset = 1
 
   function doUpdate() {
     newData = newData.filter(function(d){
-      if (!(d.key == "1 Line" || d.key == "2 Line" || d.key == "3 Line")){
+      var matchingKeys = []
+      for (var i = 0; i<selectedTrains.length; i++){
+        var keyString = selectedTrains[i] + " Line"
+        matchingKeys.push(keyString)
+      }
+      if (!matchingKeys.includes(d.key)) {
+        if (d.value !== 100){
+          d.actual = d.value
+        }
         d.value = 100
+      }
+      else {
+        d.value = d.actual
       }
       return 1
     })
-    console.log(newData)
     var select_data = newData.map(function(d){
         return{
         date: d.date,
@@ -31,9 +43,6 @@ $(document).ready(function() {
 
   }
   $(".train").click(function(d) {
-
-
-
     var id = $(this).attr("id")
     if (canReset) {
       canReset = 0
@@ -43,7 +52,6 @@ $(document).ready(function() {
         $("#"+allTrains[i]).removeClass("selected")
       }
       $("#"+id).addClass("selected")
-      doUpdate();
     }
 
     else {
@@ -56,18 +64,18 @@ $(document).ready(function() {
         $("#" + id).addClass("selected")
         selectedTrains.push(id)
       }
-
     }
+    doUpdate()
 
   })
 
-})
-var allTrains = ["1", "2", "3", "4", "5", "6", "A", "B", "C", "D", "E", "F"]
-var selectedTrains = ["1", "2", "3", "4", "5", "6", "A", "B", "C", "D", "E", "F"]
+
 var width = 1050;
 var height = 700;
 var newData;
-var csvpath = "https://gist.githubusercontent.com/soybean/d04d59bd6db77f1a73ee7cfe227c2c77/raw/c418303aefc16fc3be096cbc5fb37f96a62d91f5/test2.csv"
+var csvpath = "https://gist.githubusercontent.com/soybean/58ff494a98ca73d381089cb660382f8a/raw/48b74b9584d789631b565959c75824252ce61972/test.csv"
+//var csvpath = "https://gist.githubusercontent.com/soybean/d04d59bd6db77f1a73ee7cfe227c2c77/raw/c418303aefc16fc3be096cbc5fb37f96a62d91f5/test2.csv"
+var csvpath = "https://gist.githubusercontent.com/soybean/9c7090ce3ebbf1676d7ce0c9d1ec6037/raw/17b0f07469b25b6c0f0c5749a73270e1a762e7ca/test3.csv"
 var svg = d3.select("#tiny_mouse")
   .attr("width", width)
   .attr("height", height)
@@ -75,7 +83,7 @@ var svg = d3.select("#tiny_mouse")
 var x = d3.time.scale()
   .range([0, width]);
 var y = d3.scale.linear()
-  .range([0, 100]);
+  .range([0, height-10]);
 
 var stack = d3.layout.stack()
   .offset("silhouette")
@@ -92,68 +100,47 @@ var stack = d3.layout.stack()
         return x(d.date);
       })
       .y0(function(d) {
-        if (Number.isNaN(y(+d.y0))){
-        }
-        return d.y0*1.5;
-        //return y(d.y0)
+        return d.y0*0.5;
       })
       .y1(function(d) {
-        if (Number.isNaN(d.y0) || Number.isNaN(d.y)){
-          console.log("NaN detected... in other one")
-          console.log(d)
-        }
-        if (Number.isNaN(y(d.y0 + d.y))){
-          //console.log(d.y0 + d.y)
-          //console.log("y is NaN!")
-        }
-        return (d.y0 + d.y)*1.5;
-        return y(d.y0 + d.y);
+        return (d.y0 + d.y)*0.5;
       });
 
 d3.csv(csvpath, function(data) {
-    //console.log(data[0]["PARENT_SEQ"])
-    data.forEach(function(d){
-
-      if (+d.INDICATOR_SEQ !== 391698 && +d.INDICATOR_SEQ !==391715){
+  data.forEach(function(d){
+    if (+d.INDICATOR_SEQ !== 391698 && +d.INDICATOR_SEQ !==391715){
       d.date = new Date(+d.PERIOD_YEAR, +d.PERIOD_MONTH)
-
-        d.value = +d['MONTHLY_ACTUAL']
-        d.key = d['INDICATOR_NAME'].substring(17)
-      }
-    })
-    newData = data.filter(function(d){
-      return 1
-      //return d.key == "1 Line" || d.key == "2 Line" || d.key == "3 Line"
-    })
-    data = newData
-    var select_data = data.map(function(d){
-        return{
-        date: d.date,
-        value: 100- d.value,
-        key: d.key
-      }
-    })
-    var layers = stack(nest.entries(select_data));
-    x.domain(d3.extent(data, function(d) { return d.date; }));
-    y.domain([0, d3.max(data, function(d) { return d.y0 + d.y; })]);
-    svg.selectAll(".layer")
-        .data(layers)
-      .enter().append("path")
-        .attr("class", "layer")
-        .attr("d", function(d, i) {
-          return area(d.values)
-        })
-        .style("fill", function(d,i) {
-          var color = d3.interpolateViridis(i*0.16666)
-          return color
-        })
-        // .transition()
-        //   .duration(1500)
-        //   .style("fill", function(d,i){
-        //     var random = Math.random()
-        //     return d3.interpolateViridis(random)
-        //
-        //   })
+      d.value = +d['MONTHLY_ACTUAL']
+      d.actual = d.value
+      d.key = d['INDICATOR_NAME'].substring(17)
+    }
+  })
+  newData = data.filter(function(d){
+    return 1
+    //return d.key == "1 Line" || d.key == "2 Line" || d.key == "3 Line"
+  })
+  data = newData
+  var select_data = data.map(function(d){
+    return{
+      date: d.date,
+      value: 100- d.value,
+      key: d.key
+    }
+  })
+  var layers = stack(nest.entries(select_data));
+  x.domain(d3.extent(data, function(d) { return d.date; }));
+  y.domain([0, 1000])
+  svg.selectAll(".layer")
+  .data(layers)
+  .enter().append("path")
+  .attr("class", "layer")
+  .attr("d", function(d, i) {
+    return area(d.values)
+  })
+  .style("fill", function(d,i) {
+    var color = d3.interpolateViridis(i*0.05263157894)
+    return color
+  })
 
     svg.selectAll(".layer")
     .on("mouseover", function(d) {
@@ -162,4 +149,5 @@ d3.csv(csvpath, function(data) {
     .on("mouseout", function(d) {
       d3.select(this).style("opacity", 1)
     })
+})
 })
