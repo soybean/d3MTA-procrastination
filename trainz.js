2305af890efaac2e16d3db02938379cd6bf8e8c1
Melanie Sawyer <melaniensawyer@gmail.com>
2019-10-07 19:23:24 -0400

    add scaling for features

--- trainz.js
+++ trainz.js
@@ -1,6 +1,7 @@
 $(document).ready(function() {
   var canReset = 1
-
+  var totalPixelsCurrentlyDisplayed = 0;
+  var allPixels = [29.31, 56.64, 40.65, 56.61, 52.88, 57.83, 34.12, 51.53, 51, 46.57, 54.43, 47.6, 59.69, 35.92, 35.29, 7.879, 36.22, 37.97]
   function doUpdate() {
     newData = newData.filter(function(d){
       var matchingKeys = []
@@ -19,6 +20,7 @@ $(document).ready(function() {
       }
       return 1
     })
+    console.log(newData)
     var select_data = newData.map(function(d){
         return{
         date: d.date,
@@ -66,12 +68,13 @@ $(document).ready(function() {
     doUpdate()
 
   })
-})
+
 var allTrains = ["1", "2", "3", "4", "5", "6", "A", "B", "C", "D", "E", "F", "G", "J", "L", "N", "Q", "R"]
 var selectedTrains = ["1", "2", "3", "4", "5", "6", "A", "B", "C", "D", "E", "F", "G", "J", "L", "N", "Q", "R"]
 var width = 1050;
-var height = 600;
+var height = 525;
 var newData;
+var totalPix = 0
 var csvpath = "https://gist.githubusercontent.com/soybean/58ff494a98ca73d381089cb660382f8a/raw/48b74b9584d789631b565959c75824252ce61972/test.csv"
 //var csvpath = "https://gist.githubusercontent.com/soybean/d04d59bd6db77f1a73ee7cfe227c2c77/raw/c418303aefc16fc3be096cbc5fb37f96a62d91f5/test2.csv"
 var csvpath = "https://gist.githubusercontent.com/soybean/9c7090ce3ebbf1676d7ce0c9d1ec6037/raw/17b0f07469b25b6c0f0c5749a73270e1a762e7ca/test3.csv"
@@ -93,17 +96,44 @@ var stack = d3.layout.stack()
       .y(function(d) { return d.value; });
   var nest = d3.nest()
     .key(function(d) { return d.key; });
+
+  function getScale() {
+    var totalPixels = 0
+    for (var i = 0; i < selectedTrains.length; i++) {
+      var idx = allTrains.indexOf(selectedTrains[i])
+      var pixels = allPixels[idx]
+      totalPixels += pixels
+    }
+    console.log(totalPixels)
+    return 350/totalPixels
+
+    return (-1*8/17)*selectedTrains.length + (305/34)
+    if(selectedTrains.length == 1) {
+      scale = 8.5
+    }
+    else {
+      scale = 0.5
+    }
+    return scale
+  }
+  scale = getScale()
   var area = d3.svg.area()
       .interpolate("cardinal")
       .x(function(d) {
         return x(d.date);
       })
       .y0(function(d) {
-        return d.y0*0.5;
+        scale = getScale()
+        return d.y0*scale;
       })
       .y1(function(d) {
-        return (d.y0 + d.y)*0.5;
+        scale = getScale()
+        return (d.y0 + d.y)*scale;
       });
+
+function calculateLastPointY(){
+
+}
 
 d3.csv(csvpath, function(data) {
   data.forEach(function(d){
@@ -127,6 +157,7 @@ d3.csv(csvpath, function(data) {
     }
   })
   var layers = stack(nest.entries(select_data));
+  //console.log(totalPix)
   x.domain(d3.extent(data, function(d) { return d.date; }));
   y.domain([0, 1000])
   svg.selectAll(".layer")
@@ -148,4 +179,5 @@ d3.csv(csvpath, function(data) {
     .on("mouseout", function(d) {
       d3.select(this).style("opacity", 1)
     })
+})
 })
